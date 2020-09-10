0fbf813deddf9aa6804d7fc02144cf86a2c3b723
CaelestisZ <niranjanbk532@gmail.com>
2020-09-10 09:51:42 +0530

    PeriodicRefresh: added decrementSeatCount functionality
    Signed-off-by: CaelestisZ <niranjanbk532@gmail.com>

--- PeriodicRefresh/index.php
+++ PeriodicRefresh/index.php
@@ -1,5 +1,8 @@
 <?php
     extract($_GET);
+    if($jsonarr!='{}') {
+        file_put_contents('count.json', $jsonarr);
+    }
     $json_file = file_get_contents('count.json');
     $json = json_encode(json_decode($json_file, true), true);
     echo $json;
